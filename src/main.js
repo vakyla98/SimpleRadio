f39f5d58ff523e7c5f4b0cae597130118ce3eaa1
vakyla98 <vakyla98@ukr.net>
2020-06-11 14:41:50 +0300

    reconnect vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 
 
 import '@/style/style.scss'
@@ -9,6 +8,5 @@ import '@babel/polyfill'
 Vue.config.productionTip = false
 
 new Vue({
-    vuetify,
     render: h => h(App)
 }).$mount('#app')
