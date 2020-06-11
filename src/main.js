df85312999f31715be0edeb4d4f93c9d7b130c01
vakyla98 <vakyla98@ukr.net>
2020-06-11 14:45:03 +0300

    Connect Vuetify minifyer

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 
 import '@/style/style.scss'
 import '@babel/polyfill'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
+    vuetify,
     render: h => h(App)
 }).$mount('#app')
