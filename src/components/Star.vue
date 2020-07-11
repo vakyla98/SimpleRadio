fe2cc10ee300cb9e7cb677557feb96b3ccedee1c
vakyla98 <vakyla98@ukr.net>
2020-07-11 20:52:13 +0300

    Add icons to star

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="star"></div>
+    <div class="star">
+        <v-icon>
+            {{ isFavourite ? icons.favourite : icons.notFavourite }}
+        </v-icon>
+    </div>
 </template>
-
+<script>
+import { mdiStar, mdiStarOutline } from '@mdi/js'
+export default {
+    name: 'Star',
+    data() {
+        return {
+            icons: {
+                favourite: mdiStar,
+                notFavourite: mdiStarOutline,
+            },
+        }
+    },
+    props: {
+        isFavourite: { type: Boolean, default: false },
+    },
+}
+</script>
 <style lang="scss">
 .star {
     position: absolute;
     align-self: flex-end;
-    transform: translate(-10px, 10px);
-    width: 10px;
-    height: 10px;
-    border: 2px solid black;
-    background-color: yellow;
+    transform: translate(-8px, 8px);
 }
 </style>
